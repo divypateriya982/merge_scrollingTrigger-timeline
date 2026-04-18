b178af486cb5c88c6df268981dbbaf2192ec09f3
Divy Pateriya <pateriyadivy123@gmail.com>
2026-04-18 14:31:31 +0530

    section1 animation updated

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function page1animation() {
     tl.from("nav h1, .part-2 h4, .part-2 button", {
         y: -30,
         opacity: 0,
-        delay: 0.4,
-        duration: 0.5,
+        delay: 0.2,
+        duration: 0.4,
         stagger: 0.15
     });
     tl.from('.center-part1 h1', {
         x: -50,
-        duration: 0.5,
+        duration: 0.4,
         opacity: 0
     });
     tl.from('.center-part1 p', {
         x: -20,
-        duration: 0.4,
+        duration: 0.3,
         opacity: 0
     });
     tl.from('.center-part1 button', {
@@ -23,13 +23,13 @@ function page1animation() {
     });
     tl.from('.center-part2 img', {
         x: 100,
-        duration: 0.5,
+        duration: 0.4,
         opacity: 0
     }, "-=1"); // yaha timline mein delay shi se kaam nhi karta isliye isliye -= se utne seconds animation pehle run hoga
     tl.from('.section1bottom img', {
         y: 30,
         opacity: 0,
-        duration: 0.4,
+        duration: 0.2,
         stagger: 0.15
     });
 }
